f3c177cfee62aa9b942610bb62b32e5db970c55e
Alex Miagotin <oleksandr.external@dnascript.co>
2024-10-23 18:04:52 +0000

    improved conan build script

--- scripts/conan_build.sh
+++ scripts/conan_build.sh
@@ -4,4 +4,16 @@
 # conan profile path default
 # conan install . --output-folder=build --build=missing
 
+if ! command -v conan &>/dev/null; then
+    echo "conan could not be found, installing..."
+    python3 -m ensurepip --default-pip
+    pip3 install conan
+fi
+
+# check if conan profile exists using conan profile list and grep   
+if ! conan profile list | grep -q "default"; then
+    echo "conan profile not found, creating default..."
+    conan profile detect
+fi
+
 conan install . -c tools.cmake.cmaketoolchain:generator=Ninja --output-folder=build --build=missing --settings=compiler.cppstd=20
