70cfe07621168da9817ad19c831f203d0ae03cdf
Alex Miagotin <oleksandr.external@dnascript.co>
2024-10-24 18:55:54 +0000

    update build file

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
 cmake -S . -B ./build
+cmake --build build --verbose
 
-unameOut="$(uname -s)"
-case "${unameOut}" in
-    Linux*)     
-        machine=Linux
-        cmake --build build # --verbose
-        echo "Linux build ran";;
-    Darwin*)    
-        machine=Mac;;
-    CYGWIN*)    
-        machine=Cygwin;;
-    MINGW*)     
-        cmake --build build -G "Visual Studio 17 2022" -A x64 -DCMAKE_TOOLCHAIN_FILE="conan_toolchain.cmake"
-        machine=MinGw;;
-    MSYS_NT*)   
-        machine=Git;;
-    *)          
-        machine="UNKNOWN:${unameOut}"
-esac
+# unameOut="$(uname -s)"
+# case "${unameOut}" in
+#     Linux*)     
+#         machine=Linux
+#         cmake --build build --verbose
+#         echo "Linux build ran";;
+#     Darwin*)    
+#         machine=Mac;;
+#     CYGWIN*)    
+#         machine=Cygwin;;
+#     MINGW*)     
+#         cmake --build build --verbose
+#         machine=MinGw;;
+#     MSYS_NT*)   
+#         machine=Git;;
+#     *)          
+#         machine="UNKNOWN:${unameOut}"
+# esac
